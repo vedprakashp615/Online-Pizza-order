b2f72b6c2e799068fc87745cf6478721adb59680
vedprakashp615 <56842254+vedprakashp615@users.noreply.github.com>
2020-04-08 00:53:56 +0530

    Online Pizza order

---
+++ javascript.js
@@ -0,0 +1,19 @@
+const name = document.getElementById('name')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submit' , (e) =>{
+    let messages = []
+    
+    if(password.value === "12345678" && name.value === "vedprakash@gmail.com"){
+      messages.push("sucessfull")
+      window.location.href = "details.html"
+    }
+
+
+    if(messages.length > 0){
+        e.preventDefault()   
+        errorElement.innerHTML = messages.join(',')
+    }
+})
